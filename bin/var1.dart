f9440464b6541abb588b018c7b0112768cbd66ae
harikrishnan <kphari891@gmail.com>
2022-11-22 16:17:15 +0530

    commit

---
+++ bin/var1.dart
@@ -0,0 +1,10 @@
+import 'package:var1/var1.dart' as var1;
+
+void main(List<String> arguments) {
+var name= 'krishna';
+print(name);
+dynamic name2='hari';
+print(name2);
+name2 =123;
+print(name2);
+}
---
+++ test/var1_test.dart
@@ -0,0 +1,8 @@
+import 'package:var1/var1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
